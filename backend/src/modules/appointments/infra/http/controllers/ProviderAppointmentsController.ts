a2222de7d8e8e69a3d4ac6c8144e552d459b6eca
andrejr971 <andre.souza64@fatec.sp.gov.br>
2020-08-13 14:49:39 -0300

    Back-end adjustment Api 1.0

--- backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { classToClass } from 'class-transformer';
 
 import { container } from 'tsyringe';
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
@@ -7,19 +8,19 @@ export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
 
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listProviderAppointmentsService = container.resolve(
       ListProviderAppointmentsService,
     );
 
     const appointments = await listProviderAppointmentsService.execute({
-      day,
-      month,
-      year,
+      month: Number(month),
+      year: Number(year),
+      day: Number(day),
       provider_id,
     });
 
-    return response.json(appointments);
+    return response.json(classToClass(appointments));
   }
 }
